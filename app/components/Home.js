295388658df07ef58224d96330e6594df8430fba
standardorcawolf <80548600+standardorcawolf@users.noreply.github.com>
2021-03-14 10:34:30 -0500

    feat: import bitwarden passwords
    Supports importing bitwarden passwords format.

--- app/components/Home.js
+++ app/components/Home.js
@@ -5,11 +5,14 @@ import ViewEntries from './ViewEntries';
 import ConfirmDialog from './ConfirmDialog';
 import DataErrorAlert from './DataErrorAlert';
 import { EditorKit, EditorKitDelegate } from 'sn-editor-kit';
+import DataImportEntry from './DataImportEntry';
 
 const initialState = {
   text: '',
+  jsonPrepImport: '',
   entries: [],
   parseError: false,
+  importMode: false,
   editMode: false,
   editEntry: null,
   confirmRemove: false,
@@ -21,6 +24,8 @@ export default class Home extends React.Component {
     super(props);
     this.configureEditorKit();
     this.state = initialState;
+    this.attemptImport = this.attemptImport.bind(this);
+    this.updateTextState = this.updateTextState.bind(this);
   }
 
   configureEditorKit() {
@@ -59,7 +64,7 @@ export default class Home extends React.Component {
           };
         }
       },
-      clearUndoHistory: () => {},
+      clearUndoHistory: () => { },
       getElementsBySelector: () => []
     });
 
@@ -78,6 +83,19 @@ export default class Home extends React.Component {
   addEntry = entry => {
     this.setState(state => {
       const entries = state.entries.concat([entry]);
+      this.saveNote(entries);
+
+      return {
+        editMode: false,
+        editEntry: null,
+        entries
+      };
+    });
+  };
+
+  addEntries = newEntries => {
+    this.setState(state => {
+      const entries = state.entries.concat(newEntries);
       this.saveNote(entries);
 
       return {
@@ -121,6 +139,51 @@ export default class Home extends React.Component {
       editEntry: null
     });
   };
+
+  onImportNew = () => {
+    this.setState({
+      importMode: true,
+    })
+  }
+
+  attemptImport = () => {
+    let attemptedImport = [];
+    let rawJSON = null;
+    //We're going w/ a Bitwarden import here and add extras under the raw: {} section
+
+    // currently .service, .account, .secret, .notes
+    // we're going to add another field .raw which will allow us to progressively update as we
+    // add features
+    try {
+      rawJSON = JSON.parse(this.state.jsonPrepImport);
+    } catch (error) {
+      console.log("Error occured parsing a note");
+    }
+
+    if (rawJSON) {
+      //We're going to look under items[]
+      for (let i = 0; i < rawJSON.items.length; i++) {
+        let item = {};
+        item.service = rawJSON.items[i].name;
+        item.account = rawJSON.items[i].login.username;
+        item.secret = rawJSON.items[i].login.password;
+        item.notes = rawJSON.items[i].notes;
+        item.raw = rawJSON.items[i];
+        attemptedImport.push(item);
+      }
+      this.addEntries(attemptedImport);
+    }
+
+    this.setState({
+      importMode: false
+    })
+  }
+
+  updateTextState = (text) => {
+    this.setState({
+      jsonPrepImport: text
+    });
+  }
 
   onEdit = id => {
     this.setState(state => ({
@@ -179,9 +242,8 @@ export default class Home extends React.Component {
     return (
       <div className="sn-component">
         <div
-          className={`auth-copy-notification ${
-            this.state.displayCopy ? 'visible' : 'hidden'
-          }`}
+          className={`auth-copy-notification ${this.state.displayCopy ? 'visible' : 'hidden'
+            }`}
         >
           <div className="sk-panel">
             <div className="sk-font-small sk-bold">
@@ -190,10 +252,20 @@ export default class Home extends React.Component {
           </div>
         </div>
         {this.state.parseError && <DataErrorAlert />}
+        {this.state.importMode &&
+          <DataImportEntry
+            onUpdate={(text) => this.updateTextState(text)}
+            onConfirm={() => this.attemptImport}
+            onCancel={() => this.setState({ importMode: false })}
+          />
+        }
         <div id="header">
           <div className="sk-button-group">
             <div onClick={this.onAddNew} className="sk-button info">
               <div className="sk-label">Add New</div>
+            </div>
+            <div onClick={this.onImportNew} className="sk-button info">
+              <div className="sk-label">Import</div>
             </div>
           </div>
         </div>
