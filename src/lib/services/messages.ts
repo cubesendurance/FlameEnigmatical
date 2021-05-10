daed88e3d5f2b77de9f66aaaf65a9b14b93ac7de
Cubes Endurance <iloveyou@donotemailme.com>
2021-05-10 09:45:35 -0500

    feat: update messages

---
+++ src/lib/services/messages.ts
@@ -0,0 +1,14 @@
+export const COPY_MESSAGE = {
+    ACCOUNT : {
+        SUCCESS : "Copied Account",
+        FAIL : "No Account To Copy"
+    },
+    PASSWORD : {
+        SUCCESS : "Copied Password",
+        FAIL : "No Password To Copy"
+    },
+    SECONDFACTOR : {
+        SUCCESS : "Copied Second Factor",
+        FAIL : "No Second Factor To Copy"
+    }
+};
